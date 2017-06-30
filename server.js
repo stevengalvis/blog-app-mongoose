ea6f7c9771fef577de82318f090fb75f0a9b5261
stevengalvis <steven.galvis@gmail.com>
2017-06-30 08:28:21 -0400

    added GET request

--- server.js
+++ server.js
@@ -4,5 +4,26 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
+const {PORT, DATABASE_URL} = require('./config');
+const {Blog} = require('./models');
+
 const app = express();
 app.use(bodyParser.json());
+
+// send back all post from DATABASE_URL
+app.get('/blogs', (req, res) => {
+  Blog
+    .find()
+    .exec()
+    .then(blogs => {
+      res.json({
+        blogs: blogs.map(
+          (blog) => blog.apiRepr())
+      });
+    })
+    .catch(
+      err => {
+        console.error(err);
+        res.status(500).json({message: 'Internal server error'});
+      });
+});
