ce11a9bc55fa826e46fb085774a0a298d82bdff6
stevengalvis <steven.galvis@gmail.com>
2017-06-30 07:56:44 -0400

    added config.js

--- server.js
+++ server.js
@@ -6,10 +6,3 @@ mongoose.Promise = global.Promise;
 
 const app = express();
 app.use(bodyParser.json());
-
-
-
-
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});
